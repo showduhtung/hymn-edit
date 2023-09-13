3f1262f28199b3446e5fec2063c25ffcc7066ec1
Shaun <shaun.tung@gmail.com>
2023-09-13 08:34:03 +0800

    initial update

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,121 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import {
+  Box,
+  Checkbox,
+  Container,
+  Divider,
+  List,
+  ListItemButton,
+  Paper,
+  Typography,
+  styled,
+} from "@mui/material";
+import { useToggle } from "@uidotdev/usehooks";
+import { DragEventHandler, useState } from "react";
+
+const StyledContainer = styled(Container)(({ theme }) => ({
+  padding: "24px",
+  [theme.breakpoints.up("lg")]: { maxWidth: "95vw" },
+}));
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <StyledContainer>
+      <Box display="flex" gap="24px" height="100dvh">
+        <Paper
+          elevation={2}
+          sx={{ width: "30%", maxWidth: "450px", p: "24px" }}
+        >
+          <Box px="12px">
+            <Typography fontSize={24} sx={{ textDecoration: "underline" }}>
+              Hymn List
+            </Typography>
+            <Box height="2px" />
+            <Typography fontSize={12}>
+              Please drag JSON file(s) into this box
+            </Typography>
+          </Box>
+          <Divider sx={{ mt: "24px" }} />
+          <MusicList />
+        </Paper>
+        <Paper elevation={2} sx={{ flex: 1, padding: "24px" }}></Paper>
+      </Box>
+    </StyledContainer>
   );
 }
 
 export default App;
+
+const MusicList = () => {
+  const [data] = useState(["144. Wait on God and Trust Him", 2, 3]);
+  const [selected, setSelected] = useState(-1);
+  const [on, toggle] = useToggle(false);
+
+  function preventDefaults(e: DragEvent) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  function handleDrop(e: DragEvent) {
+    preventDefaults(e);
+    toggle(false);
+    const { files } = e.dataTransfer ?? { files: [] as FileList };
+    console.log("hanlding drop", files);
+    handleFiles(files);
+  }
+
+  function handleFiles(files: FileList) {
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      if (file.type === "application/json") {
+        console.log("File is a JSON file:", file.name);
+      } else {
+        console.log("File is NOT a JSON file:", file.name);
+      }
+    }
+  }
+
+  return (
+    <List
+      style={{
+        opacity: on ? 0.5 : 1,
+        backgroundColor: on ? "rgba(0,0,0,0.1)" : "transparent",
+        borderStyle: on ? "dashed" : "none",
+        borderRadius: 5,
+      }}
+      onDragEnter={preventDefaults}
+      onDragOver={(e) => {
+        preventDefaults(e);
+        toggle(true);
+      }}
+      onDragLeave={(e) => {
+        preventDefaults(e);
+        toggle(false);
+      }}
+      onDrop={handleDrop}
+    >
+      {data.map((item, idx) => (
+        <ListItemButton
+          key={item}
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            borderRadius: 2,
+            ":hover": { backgroundColor: "transparent" },
+          }}
+          onClick={() => setSelected(idx)}
+          selected={selected === idx}
+          disableRipple
+        >
+          <Typography>{item}</Typography>
+          <Checkbox disabled />
+        </ListItemButton>
+      ))}
+    </List>
+  );
+};
+
+const useDnd = () => {};
