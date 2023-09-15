7dff8471aac9ac8b273c59b5fe9ef25751944257
Shaun <shaun.tung@gmail.com>
2023-09-15 09:14:42 +0800

    quickly moving things around

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "@fontsource/inter";
 
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 import { Box, Container, Paper, styled } from "@mui/material";
-import { Input, Button, ButtonGroup, Typography, Stack } from "@mui/joy";
-import { HymnList } from "./components";
-import { RestartAlt } from "@mui/icons-material";
+import { Typography } from "@mui/joy";
+import { HymnForm, HymnList } from "./components";
 
 const StyledContainer = styled(Container)(({ theme }) => ({
   padding: "24px",
@@ -41,13 +40,7 @@ function App() {
             p: "24px",
           }}
         />
-        <Paper
-          elevation={2}
-          sx={{
-            flexGrow: 1,
-            padding: "24px",
-          }}
-        >
+        <Paper elevation={2} sx={{ flexGrow: 1, padding: "24px" }}>
           {selectedHymn ? (
             <HymnForm selectedHymn={selectedHymn} />
           ) : (
@@ -58,71 +51,6 @@ function App() {
     </StyledContainer>
   );
 }
-const HymnForm = ({ selectedHymn }: { selectedHymn: HymnType }) => {
-  const [selectedVerse, setSelectedVerse] = useState<number>(0);
-
-  const [_verseNo, ...content] = splitByBreakLine(
-    selectedHymn.verses[selectedVerse].html
-  );
-  const [formContent, setFormContent] = useState<string[]>(content);
-
-  function handleUpdateInput(idx: number) {
-    return (e: ChangeEvent<HTMLInputElement>) => {
-      const newContent = [...formContent];
-      newContent.splice(idx, 1, e.target.value);
-      setFormContent(newContent);
-    };
-  }
-
-  function handleResetLineText(idx: number) {
-    return () => {
-      console.log({ content });
-      const newContent = [...content];
-      newContent.splice(idx, 1, content[idx]);
-      setFormContent(newContent);
-    };
-  }
-
-  return (
-    <Stack spacing="24px">
-      <Typography fontSize="24px">Verse {_verseNo}</Typography>
-      <Box display="flex" gap="24px" justifyContent="space-between">
-        <Stack width="40%" spacing="8px">
-          {formContent.map((line, idx) => (
-            <Input
-              key={idx}
-              variant="plain"
-              value={line}
-              onChange={handleUpdateInput(idx)}
-              endDecorator={<RestartAlt onClick={handleResetLineText(idx)} />}
-            />
-          ))}
-        </Stack>
-        <Stack spacing="8px" border="1px solid black" padding="24px">
-          {formContent.map((line) => (
-            <Box display="flex" key={line}>
-              <Typography fontWeight={700}>{line[0]}</Typography>
-              <Typography>{line.slice(1)}</Typography>
-            </Box>
-          ))}
-        </Stack>
-      </Box>
-      <Box height="24px">
-        <ButtonGroup variant="outlined">
-          {selectedHymn.verses.map((_item, idx) => (
-            <Button
-              variant={idx === selectedVerse ? "solid" : "outlined"}
-              onClick={() => setSelectedVerse(idx)}
-              color="primary"
-            >
-              {idx + 1}
-            </Button>
-          ))}
-        </ButtonGroup>
-      </Box>
-    </Stack>
-  );
-};
 
 export default App;
 
@@ -148,11 +76,3 @@ const ex = {
     },
   ],
 };
-
-function splitByBreakLine(inputStr: string) {
-  return inputStr
-    .split("<br>")
-    .map((line) => line.trim())
-    .filter((line) => line.length > 0)
-    .map((line) => line.replace("<b>", "").replace("</b>", ""));
-}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export * from "./HymnList";
+export * from "./HymnForm";
